fc4beb674c0851cf1f96c8d9ede23fddb98a377a
Suflanton1713 <146865914+Suflanton1713@users.noreply.github.com>
2025-04-30 01:02:43 -0500

    Update DQL.sql

--- MoveFastDB/DQL.sql
+++ MoveFastDB/DQL.sql
@@ -8,21 +8,40 @@ WHERE s.ciudad = 'Medellín'  -- Cambia 'Medellín' por la ciudad que desees
 
 -- Listar los alquileres activos con información del cliente y vehículo.
 
-SELECT 
-    a.al_id,
-    c.nombre,
-    c.apellido,
-    c.telefono,
-    c.correo,
-    v.placa,
-    v.marca,
-    v.modelo,
-    v.ciudad,
-    a.fecha_inicio,
-    a.fecha_fin
-FROM alquiler a
-JOIN cliente c ON a.cc_cliente = c.cedula
-JOIN vehiculo v ON a.placa_v = v.placa
-WHERE a.fecha_fin >= CURRENT_DATE;  -- Filtra alquileres activos
+SELECT s.nombre AS sucursal, SUM(p.monto) AS ingresos_totales
+FROM sucursal s
+JOIN vehiculo v ON s.su_id = v.sucursal_id
+JOIN alquiler a ON v.placa = a.placa_v 
+JOIN pago p ON a.al_id = p.alquiler_id
+WHERE 
+    v.placa IN (
+        SELECT placa_v
+        FROM alquiler
+        GROUP BY placa_v
+        HAVING COUNT(al_id) > 3
+    )
+GROUP BY s.nombre;
+
+
+-- Filtrar solo vehículos con más de 5 alquileres (usar subconsulta)
+
+SELECT *
+FROM vehiculo v
+WHERE v.placa IN (
+    SELECT placa_v
+    FROM alquiler
+    GROUP BY placa_v
+    HAVING COUNT(*) > 5
+);
+
+
+-- Sumar los montos de todos los pagos asociados
+
+
+SELECT SUM(p.monto) AS total_pagos
+FROM pago p;
+
+
+
 
 
